128da5ae80e64ed3fe0cdffc917df9d800b0973c
Jake Gaylor <jhgaylor@gmail.com>
2015-08-30 20:08:17 -0600

    update packaging

--- package.js
+++ package.js
@@ -12,11 +12,40 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
-  api.use('jquery', 'client');
   api.addFiles([
-    //fonts & svgs
+    'svg/artifact.svg',
+    'svg/b.svg',
+    'svg/c.svg',
+    'svg/creature.svg',
+    'svg/enchantment.svg',
+    'svg/g.svg',
+    'svg/infinite.svg',
+    'svg/instant.svg',
+    'svg/land.svg',
+    'svg/multiple.svg',
+    'svg/p.svg',
+    'svg/planeswalker.svg',
+    'svg/r.svg',
+    'svg/s.svg',
+    'svg/sorcery.svg',
+    'svg/t.svg',
+    'svg/tombstone.svg',
+    'svg/u.svg',
+    'svg/w.svg',
+    'fonts/magic-font.eot',
+    'fonts/magic-font-svg',
+    'fonts/magic-font.ttf',
+    'fonts/magic-font.woff',
+    'fonts/Matrix-Bold.eot',
+    'fonts/Matrix-Bold.ttf',
+    'fonts/Matrix-Bold.woff',
+    'fonts/Mplantin.eot',
+    'fonts/Mplantin.otf',
+    'fonts/Mplantin.svg',
+    'fonts/Mplantin.ttf',
+    'fonts/Mplantin.woff'
   ], 'client', { isAsset: true });
   api.addFiles([
-    // css
+    'css/magic-font.css'
   ], 'client');
 });
